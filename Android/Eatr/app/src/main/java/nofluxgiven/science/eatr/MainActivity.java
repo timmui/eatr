4841e5a587fc0cde1e26af2fd57a6210698cf0bb
Tim Mui <timmui123@gmail.com>
2015-06-13 16:23:41 -0400

    Android should be working?

--- Android/Eatr/app/src/main/java/nofluxgiven/science/eatr/MainActivity.java
+++ Android/Eatr/app/src/main/java/nofluxgiven/science/eatr/MainActivity.java
@@ -1,38 +1,90 @@
 package nofluxgiven.science.eatr;
 
+import android.app.Activity;
+import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+
+import com.andtinder.model.CardModel;
+import com.andtinder.view.CardContainer;
+import com.andtinder.view.SimpleCardStackAdapter;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    /**
+     * This variable is the container that will host our cards
+     */
+    private CardContainer mCardContainer;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        mCardContainer = (CardContainer) findViewById(R.id.layoutview);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        Resources r = getResources();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        SimpleCardStackAdapter adapter = new SimpleCardStackAdapter(this);
 
-        return super.onOptionsItemSelected(item);
+        adapter.add(new CardModel("Title1", "Description goes here", r.getDrawable(R.drawable.picture1)));
+        adapter.add(new CardModel("Title2", "Description goes here", r.getDrawable(R.drawable.picture2)));
+        adapter.add(new CardModel("Title3", "Description goes here", r.getDrawable(R.drawable.picture3)));
+        adapter.add(new CardModel("Title4", "Description goes here", r.getDrawable(R.drawable.picture1)));
+        adapter.add(new CardModel("Title5", "Description goes here", r.getDrawable(R.drawable.picture2)));
+        adapter.add(new CardModel("Title6", "Description goes here", r.getDrawable(R.drawable.picture3)));
+        adapter.add(new CardModel("Title1", "Description goes here", r.getDrawable(R.drawable.picture1)));
+        adapter.add(new CardModel("Title2", "Description goes here", r.getDrawable(R.drawable.picture2)));
+        adapter.add(new CardModel("Title3", "Description goes here", r.getDrawable(R.drawable.picture3)));
+        adapter.add(new CardModel("Title4", "Description goes here", r.getDrawable(R.drawable.picture1)));
+        adapter.add(new CardModel("Title5", "Description goes here", r.getDrawable(R.drawable.picture2)));
+        adapter.add(new CardModel("Title6", "Description goes here", r.getDrawable(R.drawable.picture3)));
+        adapter.add(new CardModel("Title1", "Description goes here", r.getDrawable(R.drawable.picture1)));
+        adapter.add(new CardModel("Title2", "Description goes here", r.getDrawable(R.drawable.picture2)));
+        adapter.add(new CardModel("Title3", "Description goes here", r.getDrawable(R.drawable.picture3)));
+        adapter.add(new CardModel("Title4", "Description goes here", r.getDrawable(R.drawable.picture1)));
+        adapter.add(new CardModel("Title5", "Description goes here", r.getDrawable(R.drawable.picture2)));
+        adapter.add(new CardModel("Title6", "Description goes here", r.getDrawable(R.drawable.picture3)));
+        adapter.add(new CardModel("Title1", "Description goes here", r.getDrawable(R.drawable.picture1)));
+        adapter.add(new CardModel("Title2", "Description goes here", r.getDrawable(R.drawable.picture2)));
+        adapter.add(new CardModel("Title3", "Description goes here", r.getDrawable(R.drawable.picture3)));
+        adapter.add(new CardModel("Title4", "Description goes here", r.getDrawable(R.drawable.picture1)));
+        adapter.add(new CardModel("Title5", "Description goes here", r.getDrawable(R.drawable.picture2)));
+        adapter.add(new CardModel("Title6", "Description goes here", r.getDrawable(R.drawable.picture3)));
+        adapter.add(new CardModel("Title1", "Description goes here", r.getDrawable(R.drawable.picture1)));
+        adapter.add(new CardModel("Title2", "Description goes here", r.getDrawable(R.drawable.picture2)));
+        adapter.add(new CardModel("Title3", "Description goes here", r.getDrawable(R.drawable.picture3)));
+        adapter.add(new CardModel("Title4", "Description goes here", r.getDrawable(R.drawable.picture1)));
+        adapter.add(new CardModel("Title5", "Description goes here", r.getDrawable(R.drawable.picture2)));
+
+        CardModel cardModel = new CardModel("Title1", "Description goes here", r.getDrawable(R.drawable.picture1));
+        cardModel.setOnClickListener(new CardModel.OnClickListener() {
+            @Override
+            public void OnClickListener() {
+                Log.i("Swipeable Cards", "I am pressing the card");
+            }
+        });
+
+        cardModel.setOnCardDismissedListener(new CardModel.OnCardDismissedListener() {
+            @Override
+            public void onLike() {
+                Log.i("Swipeable Cards","I like the card");
+            }
+
+            @Override
+            public void onDislike() {
+                Log.i("Swipeable Cards","I dislike the card");
+            }
+        });
+
+        adapter.add(cardModel);
+
+        mCardContainer.setAdapter(adapter);
     }
 }
